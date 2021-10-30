4e13ddc95bb156939acee79bc5c7e6538e717347
Omakatayo <labedz.sebastian@gmail.com>
2021-10-30 10:38:07 +0200

    Commit

--- animal-service/src/main/java/com/ironhack/animalservice/AnimalServiceApplication.java
+++ animal-service/src/main/java/com/ironhack/animalservice/AnimalServiceApplication.java
@@ -2,8 +2,12 @@ package com.ironhack.animalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class AnimalServiceApplication {
 
     public static void main(String[] args) {
---
+++ gateway/src/main/java/com/ironhack/gateway/GatewayConfiguration.java
@@ -0,0 +1,25 @@
+package com.ironhack.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                // Lead service
+                .route(p -> p.path("/api/v1/animal/**")
+                        .uri("lb://ANIMAL-SERVICE"))
+
+                // Opportunity service
+                .route(p -> p.path("/api/v1//adopter/**")
+                        .uri("lb://ADOPTER-SERVICE"))
+
+                .build();
+    }
+}
